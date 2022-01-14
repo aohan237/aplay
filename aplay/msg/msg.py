121d6cdf98b3001f42c1287110b58f27fb758621
linziyan <aohan237@gmail.com>
2022-01-14 11:56:23 +0800

    update msg support

--- aplay/msg/msg.py
+++ aplay/msg/msg.py
@@ -6,7 +6,24 @@ class Msg(UserDict):
 
     """Docstring for Msg."""
 
-    __getattr__ = UserDict.get
+    def __getattr__(self, attr):
+        return super(Msg, self).__getitem__(attr)
+
+    def __setattr__(self, attr, value):
+        if "data" in self.__dict__:
+            self.__dict__["data"][attr] = value
+        else:
+            super(Msg, self).__setattr__(attr, value)
+
+    def __delattr__(self, attr, value):
+        if "data" in self.__dict__:
+            res = self.__dict__["data"].pop(attr)
+        else:
+            res = super(Msg, self).__delattr__(attr)
+        return res
+
+    def __repr__(self):
+        return super(Msg, self).__repr__()
 
     def __init__(self, source=None, target=None, flow_id=None, flow_index=0, body=None):
         """TODO: to be defined."""
